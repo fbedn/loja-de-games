9de6368dd83c4f3d204b2fb921c026b8da3aec38
fbedn <f_ramos@outlook.com>
2021-02-24 18:08:18 -0300

    Adicionando início do projeto Spring

---
+++ loja-de-games-api/src/main/java/com/lojadegames/fbedn/lojadegamesapi/LojaDeGamesApiApplication.java
@@ -0,0 +1,13 @@
+package com.lojadegames.fbedn.lojadegamesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeGamesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeGamesApiApplication.class, args);
+	}
+
+}
---
+++ loja-de-games-api/src/test/java/com/lojadegames/fbedn/lojadegamesapi/LojaDeGamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojadegames.fbedn.lojadegamesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeGamesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
